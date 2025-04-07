613e90156ab76f6e5d8fea020d34f370fc05e1b3
Abdul Hannan <xyaan.bati@gmail.com>
2025-04-08 00:11:23 +0500

    working on vapi endpoint

---
+++ app/api/vapi/generate/route.ts
@@ -0,0 +1,8 @@
+export async function GET() {
+  return Response.json({ success: true, data: "Thank you" }, { status: 200 });
+}
+
+export async function POST(request: Request) {
+  const {} = await request.json();
+  
+}
